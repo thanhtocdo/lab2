6ea60d0a1344788ffbe41542219dfa94a9d0a78b
thanhtocdo <thanhnguyenkim2025@gmail.com>
2024-11-19 01:39:11 +0700

    lab2

---
+++ app/src/main/java/com/example/ad2_lab2/DAO/MonhocDAO.java
@@ -0,0 +1,75 @@
+package com.example.ad2_lab2.DAO;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.ad2_lab2.Database.DBHelper;
+import com.example.ad2_lab2.model.Monhoc;
+
+import java.util.ArrayList;
+
+
+
+public class MonhocDAO {
+    private DBHelper dbHelper;
+    private SQLiteDatabase database;
+
+    public MonhocDAO(Context context) {
+        dbHelper = new DBHelper(context);
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public boolean addMonhoc(Monhoc monhoc){
+        ContentValues values = new ContentValues();
+        values.put("TITLE",monhoc.getTitle());
+        values.put("CONTENT",monhoc.getContent());
+        values.put("DATE",monhoc.getDate());
+        values.put("TYPE",monhoc.getType());
+        values.put("STATUS",monhoc.getStatus());
+        long result = database.insert("MONHOC",null,values);
+        if (result>0)
+            return true;
+        return false;
+    }
+
+    public boolean deleteMonhoc(Monhoc monhoc){
+        long check = database.delete("MONHOC","ID=?", new String[]{String.valueOf(monhoc.getId())});
+        if (check>0)
+            return true;
+        return false;
+    }
+
+    public boolean updateMonhoc(Monhoc monhoc){
+        ContentValues values = new ContentValues();
+        values.put("TITLE",monhoc.getTitle());
+        values.put("CONTENT",monhoc.getContent());
+        values.put("DATE",monhoc.getDate());
+        values.put("TYPE",monhoc.getType());
+        long checker = database.update("MONHOC",values,"ID=?",new String[]{String.valueOf(monhoc.getId())});
+        if (checker>0)
+            return true;
+        return false;
+    }
+
+    public boolean updateStatus(Monhoc monhoc){
+        ContentValues values = new ContentValues();
+        values.put("STATUS",monhoc.getStatus());
+        long row = database.update("MONHOC",values,"ID=?",new String[]{String.valueOf(monhoc.getId())});
+        if (row>0)
+            return true;
+        return false;
+    }
+
+    public ArrayList<Monhoc> getMonhoc(){
+        ArrayList<Monhoc> monhocArrayList = new ArrayList<>();
+        Cursor cursor = database.rawQuery("SELECT * FROM MONHOC",null);
+        while (cursor.moveToNext()){
+            Monhoc monhoc = new Monhoc(cursor.getString(1),cursor.getString(2),cursor.getString(3),cursor.getString(4),cursor.getInt(5));
+            monhoc.setId(cursor.getInt(0));
+            monhocArrayList.add(monhoc);
+        }
+        return monhocArrayList;
+    }
+}
---
+++ app/src/main/java/com/example/ad2_lab2/model/Monhoc.java
@@ -0,0 +1,64 @@
+package com.example.ad2_lab2.model;
+
+import java.io.Serializable;
+
+public class Monhoc {
+    private String title,content,date,type;
+    private int id, status;
+
+    public Monhoc(String title, String content, String date, String type, int status) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.type = type;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
